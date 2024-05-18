ce1c679bacbabadce5b0ea872de799c5a19eec07
s0lena <solena.work@gmail.com>
2024-05-18 11:03:19 +0300

    Displayed HomePage with Router

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 
 export const App = () => (
   <>
     <Header />
+    {/*<nav>
+        <Link to="/">Homepage</Link>
+        <span> | </span>
+        <Link to="/expenses">Expenses</Link>
+      </nav>*/}
     <Outlet />
     <Footer />
   </>
 );
+
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom';
 import { App } from './components/App';
+import { HomePage } from './pages/HomePage'
 import './global.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage/>
+      }
+    ]
   },
 ]);
 
